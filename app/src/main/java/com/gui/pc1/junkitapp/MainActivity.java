c99f9e122857d43248a7b5bb07f37ea0f691c27a
PauDenise <pauline.aquino22@gmail.com>
2019-01-01 15:34:01 +0800

    Initial commit

---
+++ app/src/main/java/com/gui/pc1/junkitapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gui.pc1.junkitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+/*helloe*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
