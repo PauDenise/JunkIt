de6a72cbe57f88388e1ebd0076d2126f652176d4
PauDenise <pauline.aquino22@gmail.com>
2019-01-02 14:41:25 +0800

    With navigation bar, plus switching between fragments.

--- app/src/main/java/com/gui/pc1/junkit/HomeActivity.java
+++ app/src/main/java/com/gui/pc1/junkit/HomeActivity.java
@@ -1,13 +1,45 @@
 package com.gui.pc1.junkit;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_maps:
+                            selectedFragment = new MapsFragment();
+                            break;
+                        case R.id.nav_chat:
+                            selectedFragment = new ChatFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
