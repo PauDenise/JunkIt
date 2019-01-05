2cdf487d05332e119749bd6c21b9e28b12181e6d
PauDenise <pauline.aquino22@gmail.com>
2019-01-06 02:54:09 +0800

    1st Groupings, Map Update

--- app/src/main/java/com/gui/pc1/junkit/MapsFragment.java
+++ app/src/main/java/com/gui/pc1/junkit/MapsFragment.java
@@ -3,56 +3,113 @@ package com.gui.pc1.junkit;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Toast;
-
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MapsFragment extends Fragment implements OnMapReadyCallback{
-
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(getContext(), "Map is Ready.", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: Map is ready.");
         mMap = googleMap;
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        }
     }
 
     private static final String TAG = "MapsFragment";
     private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final float DEFAULT_ZOOM = 15f;
+
+    //widgets
+    //private EditText mSearchText;
 
     public static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     public static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     public static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
     public Boolean mLocationPermissionsGranted = false;
-    public Boolean isPermitted = false;
     public GoogleMap mMap;
+    public FusedLocationProviderClient mFusedLocationProviderClient;
+
+
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if(isServicesOK()){
-            getLocationPermission();
-
-        }
+            //mSearchText = getView().findViewById(R.id.input_search);
+            getLocationPermission(); }
         return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: Getting the device's current  location. ");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        try{
+            if(mLocationPermissionsGranted){
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                            if(task.getResult()!=null) {
+                                Log.d(TAG, "onComplete: Found location!");
+                                Location currentLocation = (Location) task.getResult();
+                                LatLng coordinates = new LatLng(currentLocation.getLatitude(),
+                                        currentLocation.getLongitude());
+                                moveCamera(coordinates, DEFAULT_ZOOM);
+                                //mMap.addMarker(new MarkerOptions().position(coordinates).title("HOME"));
+                            }else{
+                                Log.d(TAG, "onComplete: Current location is Null.");
+                                Toast.makeText(getContext(), "Unable to get current location.", Toast.LENGTH_SHORT).show();
+                            }
+                    }
+                });
+            }
+
+        }catch(SecurityException e){
+            Log.d(TAG, "getDeviceLocation: SecurityException: "+e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCamera: Moving camera to: Lat:"+latLng.latitude+", Lng:"+latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     public Boolean isServicesOK() {
