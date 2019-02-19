03706b616cf2b7a61efd96d99f35a95832489fe0
PauDenise <pauline.aquino22@gmail.com>
2019-02-19 08:07:35 +0800

    FB Connect with firebase na.

--- app/src/main/java/com/gui/pc1/junkit/ProfileFragment.java
+++ app/src/main/java/com/gui/pc1/junkit/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.gui.pc1.junkit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +8,53 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
+
+    private FirebaseAuth firebaseAuth;
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        Button logoutButton = v.findViewById(R.id.buttonLogout);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                LoginManager.getInstance().logOut();
+
+                // Check if user is signed in (non-null) and update UI accordingly.
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                if(currentUser==null){
+                    updateUI();
+                }
+            }
+        });
+
+        return v;
+
+
+
+
+    }
+
+    private void updateUI() {
+        Toast.makeText(getContext(), "You're logged out.", Toast.LENGTH_SHORT).show();
+
+        Intent accountIntent = new Intent(getContext(), MainActivity.class);
+        startActivity(accountIntent);
+
     }
 }
